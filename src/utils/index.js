7b3a668ec8eabf30c9434c079689630550e366ad
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-07-20 20:36:14 +0700

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   const host = process.env.REACT_APP_API_HOST;
   const port = process.env.REACT_APP_API_PORT;
 
-  if (port === 443) {
+  if (port == 443) {
     return `https://${host}`
   }
 
